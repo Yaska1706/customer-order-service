a586839d51914a504a4672126d1aaa4d42f50864
Yasin Kinyua <yasinkinyua@gmail.com>
2022-02-09 13:41:06 +0300

    Add migrations

---
+++ pkg/repository/migrations/00003_create_item_table.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF EXISTS "items" (
+    id serial PRIMARY KEY
+    created_at      timestamp with time zone default now() not null,
+    updated_at      timestamp with time zone,
+    name varchar(255) not null,
+    price integer not null,
+    
+)
